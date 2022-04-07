ad3faf19e88d6946495f2d11816f1decf6ce86e4
andriani <a.christanty@gmail.com>
2022-04-07 09:28:11 +0800

    rectified bubble plot

--- Plotly-challenge/app.js
+++ Plotly-challenge/app.js
@@ -43,7 +43,7 @@ function accessJson(jsonfile) {
   
 }
 
-//accessJson("samples.json");
+
 
 
 //build a function to utilize d3 to grab json file and put data into variables, then ultimately create charts based on those data
@@ -90,7 +90,7 @@ function createBarcharts(idSelector) {
 
     Plotly.newPlot('bar', data, layout)
   
-    
+    bubbleCharts(filteredData);
   })
   
 }
@@ -129,57 +129,28 @@ function optionChanged(newVariable) {
 dropdownEventhandler();
 
 
-function bubbleCharts() {
+function bubbleCharts(morebbdata) {
 // set the dimensions and margins of the graph
-var margin = {top: 10, right: 20, bottom: 30, left: 50},
-    width = 500 - margin.left - margin.right,
-    height = 420 - margin.top - margin.bottom;
+var trace1 = {
+  x: morebbdata[0].otu_ids,
+  y: morebbdata[0].sample_values,
+  mode: 'markers',
+  marker: {
+    size: morebbdata[0].sample_values
+  }
+};
 
-// append the svg object to the body of the page
-var svg = d3.select("#bubble")
-  .append("svg")
-    .attr("width", width + margin.left + margin.right)
-    .attr("height", height + margin.top + margin.bottom)
-  .append("g")
-    .attr("transform",
-          "translate(" + margin.left + "," + margin.top + ")");
+var data = [trace1];
 
-//Read the data
+var layout = {
+  title: `OTU ID VS SAMPLE VALUE IN $`,
+  showlegend: false,
+  height: 600,
+  width: 600
+};
 
-  // Add X axis
-  var x = d3.scaleLinear()
-    .domain([0, 10000])
-    .range([ 0, width ]);
-  svg.append("g")
-    .attr("transform", "translate(0," + height + ")")
-    .call(d3.axisBottom(x));
-
-  // Add Y axis
-  var y = d3.scaleLinear()
-    .domain([35, 90])
-    .range([ height, 0]);
-  svg.append("g")
-    .call(d3.axisLeft(y));
-
-  // Add a scale for bubble size
-  var z = d3.scaleLinear()
-    .domain([200000, 1310000000])
-    .range([ 1, 40]);
-
-  // Add dots
-  svg.append('g')
-    .selectAll("dot")
-    .data(data)
-    .enter()
-    .append("circle")
-      .attr("cx", function (d) { return x(d.gdpPercap); } )
-      .attr("cy", function (d) { return y(d.lifeExp); } )
-      .attr("r", function (d) { return z(d.pop); } )
-      .style("fill", "#69b3a2")
-      .style("opacity", "0.7")
-      .attr("stroke", "black")
-
-})
+Plotly.newPlot('bubble', data, layout);
+}
 
 
 
